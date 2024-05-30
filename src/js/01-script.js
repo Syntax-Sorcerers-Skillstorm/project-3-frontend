c7f5a76ccf726ee520342c00087784bbfc32480f
Michael_Martucci <michael.martucci@yahoo.com>
2024-05-29 23:15:48 -0400

    initial js logic update

--- src/js/01-script.js
+++ src/js/01-script.js
@@ -1,3 +1,15 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('Welcome to Project 3 Frontend');
+// })
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to Project 3 Frontend');
-})
+    const quizSelectors = document.querySelectorAll('.quiz-selector');
+
+    quizSelectors.forEach(button => {
+        button.addEventListener('click', () => {
+            const quizType = button.getAttribute('data-quiz');
+            window.location.href = `102-quiz.html?type=${quizType}`;
+        });
+    });
+});
